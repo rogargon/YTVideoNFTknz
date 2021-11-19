e43d0bb36944a6ae96d2167aa6d2a5879fa1a34d
Roberto García <roberto@rhizomik.net>
2021-11-20 00:58:55 +0100

    Organize minting form using steps

--- src/components/NFTMint.jsx
+++ src/components/NFTMint.jsx
@@ -1,24 +1,35 @@
-import {Button, Card, Input, Typography, Form, notification} from "antd";
-import {useMemo, useState} from "react";
+import {Button, Card, Input, Typography, Form, notification, Alert, Steps, Row, Col, Checkbox} from "antd";
+import React, {useMemo, useState} from "react";
 import contractInfo from "contracts/contractInfo.json";
 import Address from "components/Address/Address";
 import {useMoralis, useMoralisQuery} from "react-moralis";
 import {getEllipsisTxt} from "helpers/formatters";
 import {useEffect} from "react";
 import {useMoralisDapp} from "../providers/MoralisDappProvider/MoralisDappProvider";
+import TextArea from "antd/es/input/TextArea";
 
+const {Step} = Steps;
 const {Text} = Typography;
 
 export default function NFTMint() {
     const {Moralis} = useMoralis();
-    const { chainId } = useMoralisDapp();
+    const {chainId} = useMoralisDapp();
     const contractName = "YTVideoNFT";
-    let networkName, contract;
-    if (chainId) {
+    let networkName, contract, options;
+    if (chainId && contractInfo[parseInt(chainId)]) {
         networkName = Object.keys(contractInfo[parseInt(chainId)])[0];
         contract = contractInfo[parseInt(chainId)][networkName].contracts[contractName];
+        options = {
+            contractAddress: contract.address,
+            abi: contract.abi
+        };
+    } else if (chainId) {
+        return (<Alert message="Please, switch to one of the supported networks" type="error"/>)
     }
-    const [responses, setResponses] = useState({});
+    const [videoId, setVideoId] = useState("");
+    const [tokenId, setTokenId] = useState({});
+    const [edited, setEdited] = useState(false);
+    const [current, setCurrent] = useState(0);
 
     /**Live query */
     const {data} = useMoralisQuery("Events", (query) => query, [], {
@@ -26,11 +37,6 @@ export default function NFTMint() {
     });
 
     useEffect(() => console.log("New data: ", data), [data]);
-
-    const displayedContractFunctions = useMemo(() => {
-        if (!contract?.abi) return [];
-        return contract.abi.filter((method) => method["type"] === "function");
-    }, [contract?.abi]);
 
     const openNotification = ({message, description}) => {
         notification.open({
@@ -43,9 +49,97 @@ export default function NFTMint() {
         });
     };
 
+    const [formCheckVideoId] = Form.useForm();
+    const [formValidateVideo] = Form.useForm();
+
+    const checkVideoId = () => {
+        console.log("VideoID:", formCheckVideoId.getFieldValue("videoid"))
+        setVideoId(formCheckVideoId.getFieldValue("videoid"))
+        // TODO: Check video ID exists:
+        // GET https://www.googleapis.com/youtube/v3/videos?part=id&id=Tr5WcGSDqDg&key={YOUR_API_KEY}
+        Moralis.executeFunction({
+            functionName: "generateTokenId",
+            params: { videoId: formCheckVideoId.getFieldValue("videoid") },
+            ...options }).then((response) => {
+                setTokenId({ "videoTokenId": response.videoTokenId, "tokenId": response.tokenId })
+                setCurrent(current + 1);
+            })
+    };
+
+    const copyText = () => {
+        console.log(formValidateVideo.getFieldsValue());
+        navigator.clipboard.writeText(formValidateVideo.getFieldValue("descriptionText"));
+    };
+
+    const onCheckboxChange = (e) => { setEdited(e.target.checked); };
+
+    const validateVideo = () => {};
+
     return (
-        <div style={{margin: "auto", display: "flex", gap: "20px", marginTop: "25", width: "70vw"}}>
-            <Card
+        <div style={{width: 500, margin: "40px auto"}}>
+            <Steps current={current}>
+                <Step key={0} title="Input video"/>
+                <Step key={1} title="Video validation"/>
+                <Step key={2} title="NFT minting"/>
+            </Steps>
+            <div style={{margin: "50px 10px"}}>
+                {current === 0 && (
+                    <Form form={formCheckVideoId}
+                        name="check-videoid"
+                        layout={"horizontal"}
+                    >
+                        <Form.Item name="videoid" label="YouTube Video Identifier" required
+                                   tooltip='11 letters and numbers, including characters "-" and "_"'
+                                   rules={[{
+                                       pattern: new RegExp("^[a-zA-Z0-9_-]{11}$"),
+                                       message: 'Should be 11 letters and numbers, including "-" and "_"',
+                                   }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" onClick={checkVideoId}>Next</Button>
+                        </Form.Item>
+                    </Form>
+                )}
+                {current === 1 && (
+                    <Form form={formValidateVideo}
+                          name="validate-video"
+                          initialValues={{
+                              "descriptionText":
+                                  "Tokenized at https://ytvideonft.rhizomik.net/nfts/"+tokenId.videoTokenId }}
+                          layout={"horizontal"}
+                    >
+                        <Form.Item>
+                            <Text>To validate that you are the owner of the YouTube video to be tokenized, please, add
+                                a link to the NFT to be minted to the description of the video: </Text>
+                        </Form.Item>
+                        <Form.Item name="descriptionText">
+                            <TextArea />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="default" onClick={copyText}>Copy</Button>
+                        </Form.Item>
+                        <Form.Item>
+                            <Text>Follow this link to edit the description of the video and paste the link so it is part
+                                of the description of the video (only if it wasn't already):</Text>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="default" href={"https://studio.youtube.com/video/"+videoId+"/edit"}
+                                    target="_blank">Edit {videoId}</Button>
+                        </Form.Item>
+                        <Form.Item>
+                            <Checkbox checked={edited} onChange={onCheckboxChange}>
+                                The YouTube video description has been edited to include a link to the video NFT token identifier
+                            </Checkbox>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" disabled={!edited} onClick={validateVideo}>Next</Button>
+                        </Form.Item>
+                    </Form>
+                )}
+            </div>
+
+            {/*            <Card
                 title={
                     <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
                         Your contract: {contractName}
@@ -65,13 +159,6 @@ export default function NFTMint() {
                             if (method.name !== name) continue;
                             if (method.stateMutability === "view") isView = true;
                         }
-
-                        const options = {
-                            contractAddress: contract.address,
-                            functionName: name,
-                            abi: contract.abi,
-                            params,
-                        };
 
                         if (!isView) {
                             const tx = await Moralis.executeFunction({awaitReceipt: false, ...options});
@@ -143,7 +230,7 @@ export default function NFTMint() {
                         {getEllipsisTxt(event.attributes.transaction_hash, 14)}
                     </Card>
                 ))}
-            </Card>
+            </Card>*/}
         </div>
     );
 }
